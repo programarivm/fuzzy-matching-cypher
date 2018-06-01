54a842cf949db597683b7f576589cfaa8f08d2b6
programarivm <info@programarivm.com>
2018-06-02 00:14:09 +0100

    adding English letter frequencies

---
+++ tests/LetterFrequencyTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FuzzyMatchingOpe\Tests;
+
+use FuzzyMatchingOpe\LetterFrequency;
+use PHPUnit\Framework\TestCase;
+
+class LetterFrequencyTest extends TestCase
+{
+	/**
+     * @test
+     */
+	public function en()
+	{
+		$en = LetterFrequency::en();
+
+		$this->assertEquals(12.02, $en['e']);
+		$this->assertEquals(9.10, $en['t']);
+	}
+}
