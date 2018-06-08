f4f49889298b0b06089d8c841c5fcbc96f83d312
programarivm <info@programarivm.com>
2018-06-08 15:34:37 +0100

    LetterFreq is replaced with Alphabet\English

--- tests/Alphabet/EnglishTest.php
+++ tests/Alphabet/EnglishTest.php
@@ -1,19 +1,18 @@
 <?php
 
-namespace FuzzyMatching\Tests;
+namespace FuzzyMatching\Tests\Alphabet;
 
-use FuzzyMatching\Lang;
-use FuzzyMatching\LetterFreq;
+use FuzzyMatching\Alphabet\English;
 use PHPUnit\Framework\TestCase;
 
-class LetterFreqTest extends TestCase
+class EnglishTest extends TestCase
 {
 	/**
 	 * @test
 	 */
 	public function en()
 	{
-		$letterFreq = (new LetterFreq(Lang::EN))->get();
+		$letterFreq = (new English)->letterFreq();
 
 		$this->assertEquals(12.02, $letterFreq['e']);
 		$this->assertEquals(9.10, $letterFreq['t']);
